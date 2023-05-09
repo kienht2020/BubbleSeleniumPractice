0b9f258e520f65c9873c34b0277e06b0d84d60c6
kienht <kienht@nexlesoft.com>
2023-05-09 16:28:34 +0700

    update based on feedback from A Huy

--- src/test/java/test/ForFun2Test.java
+++ src/test/java/test/ForFun2Test.java
@@ -7,9 +7,8 @@ import org.testng.annotations.Test;
 public class ForFun2Test extends BaseTest {
     @Test
     public void Test2()  {
-        loginPage.goToLoginPage();
+
         loginPage.loginFunction(UserAccount.USER_NAME, UserAccount.PASSWORD);
-        System.out.println(loginPage.getLoginPageUrl());
     }
 
 }
